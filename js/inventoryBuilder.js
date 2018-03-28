31e91453eeae867ca08390e2850a2b08eb05ea42
Irene <gummyrene@gmail.com>
2018-03-28 10:34:57 -0700

    Added JS files

---
+++ js/inventoryBuilder.js
@@ -0,0 +1,43 @@
+
+//this is an object
+
+
+var vehicle1 = {
+  model: "Town Car",
+  manufacturer: "Lincoln",
+  miles: 300000,
+  renter: "A12k09",
+  value: 2000
+}
+
+var vehicle2 = {
+  model: "Miata",
+  manufacturer: "Mazda",
+  miles: 300000,
+  renter: "A17k09",
+  value: 2000
+}
+
+var vehicle3 = {
+  model: "Urban",
+  manufacturer: "Mazda",
+  miles: 300000,
+  renter: "A17k09",
+  value: 2000
+}
+
+var vehicle4 = {
+  model: "Classic",
+  manufacturer: "Mazda",
+  miles: 300000,
+  renter: "A17k09",
+  value: 2000
+}
+
+var vehicle5 = {
+  model: "Town Bridge",
+  manufacturer: "Ford",
+  miles: 350000,
+  renter: "A17k19",
+  value: 2000
+}
---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$( document ).ready( function () {
+
+  console.log("Hello world!");
+
+});
+
+var myFleet = [];
+
+myFleet.push(vehicle1, vehicle2, vehicle3, vehicle4, vehicle5)
+
+for (var i=0; i<6; i++){
+  print model.value();
+}
+
+
+
+myFleet[0] = vehicle1;
+myFleet[0].model;
+
+vehicle1.model = "Husky";
+console.log(vehicle1.model)
